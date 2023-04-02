85b85f5de0483166b77e84ddfd43de748e855f26
Bhazi <109984736+Bhazi@users.noreply.github.com>
2023-04-02 17:37:45 +0530

    changes with mongoose ODM

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
-const User = require("./models/user");
+// const mongoConnect = require("./util/database").mongoConnect;
+// const User = require("./models/user");
 
 const app = express();
+
+const mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -18,20 +20,31 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("6427ff3611507d6fa3b64a26")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("6427ff3611507d6fa3b64a26")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+// mongoConnect(() => {
+//   app.listen(3000);
+// });
+
+mongoose
+  .connect(
+    "mongodb+srv://Bhazi:accUp2pXejVm7DGR@cluster0.oludblc.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
